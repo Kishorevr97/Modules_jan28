879670da0aea020b6a4f4c76fdc3de5a808d5557
Kishorevr97 <107535279+Kishorevr97@users.noreply.github.com>
2025-01-28 17:27:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,10 @@
+provider "aws" {
+  region = var.aws_region
+}
 
+module "ec2_instance" {
+  source        = "./modules/ec2"
+  ami           = var.ami
+  instance_type = var.instance_type
+  instance_name = "teraa_jan27"
+}
