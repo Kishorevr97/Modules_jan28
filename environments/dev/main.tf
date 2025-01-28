e2312bbdcdd2277020ba103777d9bc817709d9b0
Kishorevr97 <107535279+Kishorevr97@users.noreply.github.com>
2025-01-28 19:53:21 +0530

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -4,5 +4,23 @@ provider "aws" {
 
 module "ec2_instance" {
   source        = "./modules/ec2"
+  ami           = var.ami
+  instance_type = var.instance_type
   instance_name = "teraa_jan27"
+}
+
+module "rds_instance" {
+  source              = "./modules/rds"
+  db_engine           = var.db_engine
+  db_instance_class   = var.db_instance_class
+  db_allocated_storage = var.db_allocated_storage
+  db_name             = var.db_name
+  db_username         = var.db_username
+  db_password         = var.db_password
+}
+
+module "s3_bucket" {
+  source           = "./modules/s3"
+  s3_bucket_name   = var.s3_bucket_name
+  bucket_name_tag  = "jan27-bucket"
 }
