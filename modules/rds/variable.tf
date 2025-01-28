8d78002c17cb16e0778fb6d9ff414738437b7c9d
Kishorevr97 <107535279+Kishorevr97@users.noreply.github.com>
2025-01-28 19:24:31 +0530

    Create variable.tf

---
+++ modules/rds/variable.tf
@@ -0,0 +1,29 @@
+variable "db_engine" {
+        description = "db engine"
+
+}
+
+variable "db_instance_class" {
+        description = "db instance class"
+
+}
+
+variable "db_allocated_storage" {
+        description = "db storage"
+
+}
+
+variable "db_name" {
+        description = "db name"
+
+}
+
+variable "db_username" {
+        description = "db username"
+
+}
+
+variable "db_password" {
+        description = "db password"
+
+}
